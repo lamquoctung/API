22d3e2a5c81414e1fa4b71dd59d2c3aa319dae00
lamquoctung <bighero6ts@gmail.com>
2020-11-17 05:05:12 +0700

    first

---
+++ hookcontextapi/src/components/BookList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { ThemeContext } from '../context/ThemeContext';
+class BookList extends Component {
+    static contextType = ThemeContext;
+    render() {
+        const { isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme ? light : dark;
+        return (
+            <div className = "book-list" style={{ color: theme.syntax, background: theme.bg}}>
+                <ul>
+                    <li style={{background:theme.ui}}>Book 1</li>
+                    <li style={{background:theme.ui}}>Book 2</li>
+                    <li style={{background:theme.ui}}>Book 3</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default BookList;
